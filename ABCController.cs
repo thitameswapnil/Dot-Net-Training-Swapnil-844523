3a62e264d04358ae236bd0e0c24258346f33b983
thitameswapnil <60810932+thitameswapnil@users.noreply.github.com>
2020-02-15 14:13:31 +0530

    send the webform data to sql database by using mvc

---
+++ ABCController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication2.Controllers
+{
+    public class ABCController : Controller
+    {
+        [HttpPost]
+        public ActionResult Index(string rn,string sk,DateTime sd,DateTime ed)
+        {
+            swapnilEntities ctx = new swapnilEntities();
+            TRM tm = new TRM();
+            tm.request_name = rn;
+            tm.skill = sk;
+            tm.startdate = sd;
+            tm.enddate = ed;
+            ctx.TRMs.Add(tm);
+            ctx.SaveChanges();
+               return View();
+        }
+        // GET: ABC
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+    }
+}
